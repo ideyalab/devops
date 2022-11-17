2da080efef3c55d51e1c4e00b279482f69c55548
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-17 18:22:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "conductor-sg" {
         from_port = 80
         to_port = 80
         protocal = "tcp"
-        cidr_block= [0.0.0.0/0]
+        cidr_block= ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
 }
     egress {
