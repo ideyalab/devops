cbfb1a6b48d5c5122611964b275aca3241bde421
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-18 13:13:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_eip" "elastic_ip" {
 resource "aws_nat_gateway" "conductor_nat" {
     allocation_id = aws_eip.elastic_ip[count.index].id
   count = var.number_of_public_subnets
-  subnet_id = aws_subnet.conductor_public_subnet[count.index].id
+  subnet_id = aws_subnet.conductor_public_subnet.id[0]
   tags = {
     Name = "nat_gateway-${var.environment}"
   }
@@ -56,32 +56,32 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 resource "aws_route_table_association" "igw_subnet_assoc" {
-  count = var.number_of_public_subnets
+  #count = var.number_of_public_subnets
   route_table_id = aws_route_table.public_route_table[count.index].id
   subnet_id = aws_subnet.conductor_public_subnet[count.index].id
 
 }
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.conductor_vpc.id
-  count = var.number_of_private_subnets
+  #count = var.number_of_private_subnets
   tags = {
     Name = "${aws_subnet.conductor_private_subnet[count.index].availability_zone}-route-table-NAT-${var.environment}"
   }
 }
 resource "aws_route_table_association" "nat_private_subnet_assoc" {
-  count = var.number_of_private_subnets
+  #count = var.number_of_private_subnets
   route_table_id = aws_route_table.private_route_table[count.index].id
   subnet_id = aws_subnet.conductor_private_subnet[count.index].id
 
 }
 resource "aws_route" "ig_public_subnet_route" {
-  count = var.number_of_public_subnets
+  #count = var.number_of_public_subnets
   route_table_id = aws_route_table.public_route_table[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.internet_gateway.id
 }
 resource "aws_route" "nat_private_subnet_route" {
-  count = var.number_of_private_subnets
+  #count = var.number_of_private_subnets
   route_table_id = aws_route_table.private_route_table[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.conductor_nat[count.index].id
