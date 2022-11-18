0b405e59b40564cca1b2af52d31c4f8ce0bf0d95
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-18 09:37:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "conductor_vpc" {
     enable_dns_support = true
     enable_dns_hostnames = true
 
-    tags {
+    tags = {
         Name = "${var.vpc_tag_name}-${var.environment}"
     }
 }
