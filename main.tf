74925f5dc690a1eb5350d605753d80349016ee0f
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-18 13:18:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_eip" "elastic_ip" {
 resource "aws_nat_gateway" "conductor_nat" {
     allocation_id = aws_eip.elastic_ip[count.index].id
   count = var.number_of_public_subnets
-  subnet_id = aws_subnet.conductor_public_subnet.id
+  subnet_id = aws_subnet.conductor_public_subnet[count.index].id
   tags = {
     Name = "nat_gateway-${var.environment}"
   }
